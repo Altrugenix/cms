d3e194f69a2d23587b36047fb7d3c55f1ec6b84c
Alchie Tagudin <chesteralantagudin@gmail.com>
2026-07-16 13:58:15 +0800

    chore: remove explicit and my-cms packages along with their configurations and artifacts

--- packages/create-app/my-cms/arche-cms.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "@arche-cms/cms";
-
-export default defineConfig({
-  database: { adapter: "sqlite" },
-  localization: { defaultLocale: "en" },
-});
--- packages/create-app/my-value/arche-cms.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "@arche-cms/cms";
-
-export default defineConfig({
-  database: { adapter: "my-value" },
-  localization: { defaultLocale: "my-value" },
-});
